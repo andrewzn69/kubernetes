27beb9a39532e911796824465874849b800d603f
Ondřej Zeman <ondrejzeman@zemn.xyz>
2025-08-02 21:06:23 +0200

    add argocd bootstrap script

---
+++ bootstrap-argocd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+helm upgrade --install argocd ./charts/argocd --namespace argocd --create-namespace
+kubectl apply -f apps/argocd.yaml  # App of Apps pattern
